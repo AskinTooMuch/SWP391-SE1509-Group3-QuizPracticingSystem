9227af32fb427b1cc3dc77e6a171cd1dabca6cc1
ChucEnvy <51465507+AskinTooMuch@users.noreply.github.com>
2021-09-16 11:23:00 +0700

    Database 2.0

--- QuizSystemDB_2.0.sql
+++ QuizSystemDB_2.0.sql
@@ -1,6 +1,6 @@
 USE master
 GO
-alter database [Quiz_System] set single_user with rollback immediate
+
 if exists (select * from sysdatabases where name='Quiz_System') drop database Quiz_System
 GO
 
@@ -10,6 +10,48 @@ GO
 USE [Quiz_System]
 GO
 
+-------------------------------------------
+CREATE TABLE dbo.[UserRole](
+	userRoleId		int	NOT NULL identity(1,1) PRIMARY KEY,
+	userRoleName	nvarchar(63) NOT NULL,
+	[status]		bit,
+)
+-------------------------------------------
+CREATE TABLE dbo.[PostCate](
+	postCateId		int	NOT NULL identity(1,1) PRIMARY KEY,
+	postCateName	nvarchar(63) NOT NULL,
+	[status]		bit,
+)
+-------------------------------------------
+CREATE TABLE dbo.[SubjectCate](
+	subjectCateId		int	NOT NULL identity(1,1) PRIMARY KEY,
+	subjectCateName	nvarchar(63) NOT NULL,
+	[status]		bit,
+)
+-------------------------------------------
+CREATE TABLE dbo.[TestType](
+	testTypeId		int	NOT NULL identity(1,1) PRIMARY KEY,
+	testTypeName	nvarchar(63) NOT NULL,
+	[status]		bit,
+)
+-------------------------------------------
+CREATE TABLE dbo.[QuizLevel](
+	quizLevelId		int	NOT NULL identity(1,1) PRIMARY KEY,
+	quizLevelName	nvarchar(63) NOT NULL,
+	[status]		bit,
+)
+-------------------------------------------
+CREATE TABLE dbo.[LessonType](
+	lessonTypeId	int	NOT NULL identity(1,1) PRIMARY KEY,
+	lessonTypeName	nvarchar(63) NOT NULL,
+	[status]		bit,
+)
+-------------------------------------------
+CREATE TABLE dbo.[DimensionType](
+	dimensionTypeId		int	NOT NULL identity(1,1) PRIMARY KEY,
+	dimensionTypeName	nvarchar(63) NOT NULL,
+	[status]		bit,
+)
 -------------------------------------------
 CREATE TABLE dbo.[User](
 	userId		int				NOT NULL identity(1,1) PRIMARY KEY,
@@ -21,6 +63,7 @@ CREATE TABLE dbo.[User](
 	gender		bit, 
 	userMobile	nchar(10),
 	[status]	bit,
+	FOREIGN KEY (roleId) REFERENCES dbo.[UserRole](userRoleId),
 )
 -------------------------------------------
 CREATE TABLE dbo.[Subject](
@@ -29,9 +72,17 @@ CREATE TABLE dbo.[Subject](
 	cateId			int				NOT NULL,
 	[description]	nvarchar(1023)	NOT NULL,
 	thumbnail		nvarchar(255),
-	userId			int				NOT NULL,
 	featuredSubject bit,
 	[status]		bit,
+	FOREIGN KEY (cateId) REFERENCES dbo.[SubjectCate](subjectCateId),
+)
+-------------------------------------------
+CREATE TABLE dbo.[SubjectExpert](
+	subjectId		int,
+	userId			int,
+	[status]		bit,
+	FOREIGN KEY (subjectId) REFERENCES dbo.[Subject](subjectId),
+	FOREIGN KEY (userId)	REFERENCES dbo.[User](userId),
 )
 -------------------------------------------
 CREATE TABLE dbo.[Lesson](
@@ -44,6 +95,8 @@ CREATE TABLE dbo.[Lesson](
 	videoLink		nvarchar(255),
 	content			nvarchar(1024),
 	[status]		bit,
+	FOREIGN KEY (subjectId)		REFERENCES dbo.[Subject](subjectId),
+	FOREIGN KEY (lessonTypeId)	REFERENCES dbo.[LessonType](lessonTypeId),
 )
 -------------------------------------------
 CREATE TABLE dbo.[Dimension](
@@ -52,6 +105,9 @@ CREATE TABLE dbo.[Dimension](
 	dimensionTypeId int				NOT NULL,
 	dimensionName	nvarchar(255)	NOT NULL,
 	[description]	nvarchar(511)	NOT NULL,
+	[status]		bit,
+	FOREIGN KEY (subjectId)			REFERENCES dbo.[Subject](subjectId),
+	FOREIGN KEY (dimensionTypeId)	REFERENCES dbo.[DimensionType](dimensionTypeId),
 )
 -------------------------------------------
 CREATE TABLE dbo.[PricePackage](
@@ -62,6 +118,7 @@ CREATE TABLE dbo.[PricePackage](
 	listPrice	money			NOT NULL,
 	salePrice	money,
 	[status]	bit,
+	FOREIGN KEY	(subjectId)		REFERENCES dbo.[Subject](subjectId),
 )
 -------------------------------------------
 CREATE TABLE dbo.[Question](
@@ -72,15 +129,19 @@ CREATE TABLE dbo.[Question](
 	content		nvarchar(1023)	NOT NULL,
 	media		nvarchar(255),
 	explanation	nvarchar(1023),
-	--type?
 	[status]	bit,
+	FOREIGN KEY (subjectId)		REFERENCES dbo.[Subject](subjectId),
+	FOREIGN KEY (dimensionId)	REFERENCES dbo.[Dimension](dimensionId),
+	FOREIGN KEY (lessonId)		REFERENCES dbo.[Lesson](lessonId),
 )
 -------------------------------------------
-CREATE TABLE dbo.[AnswerDetail](
+CREATE TABLE dbo.[Answer](
 	answerId		int				NOT NULL identity(1,1) PRIMARY KEY,
 	questionId		int				NOT NULL,
 	answerContent	nvarchar(1023)	NOT NULL,
 	isCorrect		bit				NOT NULL,
+	[status]		bit,
+	FOREIGN KEY (questionId) REFERENCES dbo.[Question](questionId),
 )
 -------------------------------------------
 CREATE TABLE dbo.[Quiz](
@@ -91,16 +152,24 @@ CREATE TABLE dbo.[Quiz](
 	quizLevelId		int				NOT NULL,
 	quizDuration	int				NOT NULL, --Minutes
 	passRate		int, --1-100?
-	[testTypeId]	int				NOT NULL,
+	testTypeId		int				NOT NULL,
 	[description]	nvarchar(1023),
 	numberQuestion	int				NOT NULL,
 	dimensionTypeId int				NOT NULL,
 	[status]		bit,
+	FOREIGN KEY (lessonId)			REFERENCES dbo.[Lesson](lessonId),
+	FOREIGN KEY (subjectId)			REFERENCES dbo.[Subject](subjectId),
+	FOREIGN KEY (quizLevelId)		REFERENCES dbo.[QuizLevel](quizLevelId),
+	FOREIGN KEY (testTypeId)		REFERENCES dbo.[TestType](testTypeId),
+	FOREIGN KEY (dimensionTypeId)	REFERENCES dbo.[DimensionType](dimensionTypeId),
 )
 -------------------------------------------
 CREATE TABLE dbo.[QuizQuestion](
 	quizId		int	NOT NULL,
 	questionId	int NOT NULL,
+	[status]	bit,
+	FOREIGN KEY (quizId)		REFERENCES dbo.[Quiz](quizId),
+	FOREIGN KEY (questionId)	REFERENCES dbo.[Question](questionId),
 )
 -------------------------------------------
 CREATE TABLE dbo.[CustomerQuiz](
@@ -109,8 +178,9 @@ CREATE TABLE dbo.[CustomerQuiz](
 	userId		int			NOT NULL,
 	score		int,
 	startedAt	datetime	NOT NULL,
-	--content?
 	[status]	bit,
+	FOREIGN KEY (quizId) REFERENCES dbo.[Quiz](quizId),
+	FOREIGN KEY (userId) REFERENCES dbo.[User](userId),
 )
 -------------------------------------------
 CREATE TABLE dbo.[TakeAnswer](
@@ -118,6 +188,10 @@ CREATE TABLE dbo.[TakeAnswer](
 	quizTakeId		int	NOT NULL,
 	questionId		int	NOT NULL,
 	answerId		int	NOT NULL,
+	[status]		bit,
+	FOREIGN KEY (quizTakeId)	REFERENCES dbo.[CustomerQuiz](quizTakeId),
+	FOREIGN KEY (questionId)	REFERENCES dbo.[Question](questionId),
+	FOREIGN KEY (answerId)		REFERENCES dbo.[Answer](answerId),
 )
 -------------------------------------------
 CREATE TABLE dbo.[Registration](
@@ -131,6 +205,9 @@ CREATE TABLE dbo.[Registration](
 	lastUpdatedBy	int				NOT NULL, --Last updated by
 	note			nvarchar(255),
 	[status]		bit,
+	FOREIGN KEY (userId) REFERENCES dbo.[User](userId),
+	FOREIGN KEY (packId) REFERENCES dbo.[PricePackage](packId),
+	FOREIGN KEY (lastUpdatedBy) REFERENCES dbo.[User](userId),
 )
 -------------------------------------------
 CREATE TABLE dbo.[Blog](
@@ -142,6 +219,9 @@ CREATE TABLE dbo.[Blog](
 	detail		nvarchar(2047)	NOT NULL,
 	postCateId	int				NOT NULL,
 	thumbnail	nvarchar(255),
+	[status]		bit,
+	FOREIGN KEY (author) REFERENCES dbo.[User](userId),
+	FOREIGN KEY (postCateId) REFERENCES dbo.[PostCate](postCateId),
 )
 -------------------------------------------
 CREATE TABLE dbo.[Slider](
@@ -150,40 +230,5 @@ CREATE TABLE dbo.[Slider](
 	[image]		nvarchar(255),
 	[link]		nvarchar(255),
 	note		nvarchar(255),
-	status		bit,
-)
--------------------------------------------
-CREATE TABLE dbo.[UserRole](
-	userRoleId		int	NOT NULL identity(1,1) PRIMARY KEY,
-	userRoleName	nvarchar(63) NOT NULL
-)
--------------------------------------------
-CREATE TABLE dbo.[PostCate](
-	postCateId		int	NOT NULL identity(1,1) PRIMARY KEY,
-	postCateName	nvarchar(63) NOT NULL
-)
--------------------------------------------
-CREATE TABLE dbo.[SubjectCate](
-	subjectCateId		int	NOT NULL identity(1,1) PRIMARY KEY,
-	subjectCateName	nvarchar(63) NOT NULL
-)
--------------------------------------------
-CREATE TABLE dbo.[TestType](
-	testTypeId		int	NOT NULL identity(1,1) PRIMARY KEY,
-	testTypeName	nvarchar(63) NOT NULL
-)
--------------------------------------------
-CREATE TABLE dbo.[QuizLevel](
-	quizLevelId		int	NOT NULL identity(1,1) PRIMARY KEY,
-	quizLevelName	nvarchar(63) NOT NULL
-)
--------------------------------------------
-CREATE TABLE dbo.[LessonType](
-	lessonTypeId	int	NOT NULL identity(1,1) PRIMARY KEY,
-	lessonTypeName	nvarchar(63) NOT NULL
-)
--------------------------------------------
-CREATE TABLE dbo.[DimensionType](
-	dimensionTypeId		int	NOT NULL identity(1,1) PRIMARY KEY,
-	dimensionTypeName	nvarchar(63) NOT NULL
+	[status]	bit,
 )
