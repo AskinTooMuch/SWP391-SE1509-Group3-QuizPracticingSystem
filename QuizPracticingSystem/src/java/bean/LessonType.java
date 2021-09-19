ca6218b9574f29a9d3aed120872a1176fe559226
DuongNH8837 <85751304+DuongNH8837@users.noreply.github.com>
2021-09-19 20:16:39 +0700

    Update LessonType.java

--- QuizPracticingSystem/src/java/bean/LessonType.java
+++ QuizPracticingSystem/src/java/bean/LessonType.java
@@ -13,6 +13,36 @@ public class LessonType {
     private int lessonTypeId;
     private String lessonTypeName;
     private boolean status;
+
+    public LessonType(int lessonTypeId, String lessonTypeName, boolean status) {
+        this.lessonTypeId = lessonTypeId;
+        this.lessonTypeName = lessonTypeName;
+        this.status = status;
+    }
+
+    public int getLessonTypeId() {
+        return lessonTypeId;
+    }
+
+    public String getLessonTypeName() {
+        return lessonTypeName;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setLessonTypeId(int lessonTypeId) {
+        this.lessonTypeId = lessonTypeId;
+    }
+
+    public void setLessonTypeName(String lessonTypeName) {
+        this.lessonTypeName = lessonTypeName;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
     
     
 }
